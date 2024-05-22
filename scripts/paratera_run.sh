2d1cbd786589e1deebffce746dec7d30fb7295ae
4o3F <4o3f@proton.me>
2024-05-22 21:24:51 +0800

    feat: add run shell script

---
+++ scripts/paratera_run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Fix font issue
+cd ~/run/yuka
+cp ~/run/yuka/assets/Arial.ttf ~/.config/Ultralytics
+
+module unload cuda/10.0
+module unload cuda/10.1
+module unload cuda/10.1u1
+module unload cuda/10.1u2
+module unload cuda/10.2
+module unload cuda/11.0
+module unload cuda/11.0u1
+module unload cuda/11.1
+module unload cuda/11.2
+module unload cuda/11.3
+module unload cuda/11.4
+module unload cuda/11.5
+module unload cuda/11.6
+module unload cuda/11.7
+module unload cuda/11.8
+module unload cuda/12.0
+module unload cuda/12.1
+module unload cuda/12.2
+module unload cuda/9.0
+module unload cuda/9.2
+
+
+module load cuda/12.2
+
+source .venv/bin/activate
+python -u train.py
--- train.py
+++ train.py
@@ -5,10 +5,12 @@ model = YOLO("yolov8x-seg-original.yaml")
 model.train(
     data="datasets/dataset_yrcc2.yaml",
     epochs=500,
-    batch=2,
+    batch=30,
     imgsz=768,
-    device="cuda",
+    device="0,1",
+    patience=40,
     save_period=50,
-    workers=0,
-    project="run"
+    # workers=0,
+    project="run",
+    mosaic=0
 )
