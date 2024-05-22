819eb11c18bc0de6a634837667638a83acca35ff
4o3F <4o3f@proton.me>
2024-05-22 16:00:21 +0800

    feat: add val_period setting

--- train.py
+++ train.py
@@ -1,3 +1,14 @@
 from ultralytics import YOLO
 
-model = YOLO("yolov8x-seg.yaml")
+model = YOLO("yolov8x-seg-original.yaml")
+
+model.train(
+    data="datasets/dataset_yrcc2.yaml",
+    epochs=500,
+    batch=2,
+    imgsz=768,
+    device="cuda",
+    save_period=50,
+    workers=0,
+    project="run"
+)
