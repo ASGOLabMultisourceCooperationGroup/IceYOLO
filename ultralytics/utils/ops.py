c7ea12367884a6b39fbe224efedb366caebd3e53
4o3F <4o3f@proton.me>
2024-05-25 14:12:45 +0800

    feat: change back to the old dataloader, for test only

--- ultralytics/utils/ops.py
+++ ultralytics/utils/ops.py
@@ -670,36 +670,26 @@ def resample_segment(points, n):
     return result
 
 
-def resample_segments(segments, n=1000):
-    resampled = []
-    i = 0
-    for s in segments:
-        result = resample_segment(s, n)
-        assert len(result) == n
-        resampled.append(np.stack(result, axis=0))
-        i += 1
-    return resampled
-
-
 # def resample_segments(segments, n=1000):
-#     """
-#     Inputs a list of segments (n,2) and returns a list of segments (n,2) up-sampled to n points each.
-#
-#     Args:
-#         segments (list): a list of (n,2) arrays, where n is the number of points in the segment.
-#         n (int): number of points to resample the segment to. Defaults to 1000
-#
-#     Returns:
-#         segments (list): the resampled segments.
-#     """
-#     for i, s in enumerate(segments):
-#         s = np.concatenate((s, s[0:1, :]), axis=0)
-#         x = np.linspace(0, len(s) - 1, n)
-#         xp = np.arange(len(s))
-#         segments[i] = (
-#             np.concatenate([np.interp(x, xp, s[:, i]) for i in range(2)], dtype=np.float32).reshape(2, -1).T
-#         )  # segment xy
-#     return segments
+#     resampled = []
+#     i = 0
+#     for s in segments:
+#         result = resample_segment(s, n)
+#         assert len(result) == n
+#         resampled.append(np.stack(result, axis=0))
+#         i += 1
+#     return resampled
+
+
+def resample_segments(segments, n=1000):
+    for i, s in enumerate(segments):
+        s = np.concatenate((s, s[0:1, :]), axis=0)
+        x = np.linspace(0, len(s) - 1, n)
+        xp = np.arange(len(s))
+        segments[i] = (
+            np.concatenate([np.interp(x, xp, s[:, i]) for i in range(2)], dtype=np.float32).reshape(2, -1).T
+        )  # segment xy
+    return segments
 
 
 def crop_mask(masks, boxes):
