4750a773c8c7eb89b591e105b813f43188916e07
4o3F <4o3f@proton.me>
2024-06-03 16:28:52 +0800

    fix: fix stream prediction

--- predict.py
+++ predict.py
@@ -2,13 +2,15 @@ from ultralytics import YOLO
 
 model = YOLO("run/yrcc2-dataloader/weights/best.pt")
 
-model.predict(
-    source="datasets/2024new_dataset",
-    imgsz=768,
-    batch=10,
-    device="0",
-    save=True,
-    stream=True,
-    project="predict",
-    name="2024new"
-)
+for result in model.predict(
+        source="datasets/2024new_dataset",
+        imgsz=768,
+        batch=10,
+        device="0",
+        save=True,
+        stream=True,
+        project="predict",
+        name="2024new"
+):
+    result.save()
+    pass
