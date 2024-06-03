0d847970571c7afc9c750dc978eae0e2d6e304e1
4o3F <4o3f@proton.me>
2024-06-03 16:18:09 +0800

    feat: add predict.py

---
+++ predict.py
@@ -0,0 +1,13 @@
+from ultralytics import YOLO
+
+model = YOLO("run/yrcc2-dataloader/weights/best.pt")
+
+model.predict(
+    source="datasets/2024new_dataset",
+    imgsz=768,
+    batch=10,
+    device="0",
+    save=True,
+    project="predict",
+    name="2024new"
+)
