6251796138c4f992d6adf6d9c031216feb0249e0
4o3F <4o3f@proton.me>
2024-06-21 22:55:20 +0800

    feat: change channel attention to senet

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -292,17 +292,23 @@ class ChannelAttention(nn.Module):
         super(ChannelAttention, self).__init__()
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
         self.max_pool = nn.AdaptiveMaxPool2d(1)
+        # self.fc = nn.Sequential(
+        #     nn.Conv2d(in_planes, in_planes // reduction, 1, bias=False),
+        #     nn.ReLU(),
+        #     nn.Conv2d(in_planes // reduction, in_planes, 1, bias=False),
+        # )
         self.fc = nn.Sequential(
-            nn.Conv2d(in_planes, in_planes // reduction, 1, bias=False),
+            nn.Linear(in_planes, in_planes // reduction, bias=False),
             nn.ReLU(),
-            nn.Conv2d(in_planes // reduction, in_planes, 1, bias=False),
+            nn.Linear(in_planes // reduction, in_planes, bias=False),
         )
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        avg_out = self.fc(self.avg_pool(x))
-        max_out = self.fc(self.max_pool(x))
-        out = avg_out + max_out
+        b, c, _, _ = x.size()
+        avg_out = self.fc(self.avg_pool(x).view(b, c))
+        max_out = self.fc(self.max_pool(x).view(b, c))
+        out = (avg_out + max_out).view(b, c, 1, 1)
         return self.sigmoid(out)
 
 
